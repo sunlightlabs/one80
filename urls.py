7249027329dc55cf1daca544e8efe164fdd55dfb
Dan Drinkard <dan.drinkard@gmail.com>
2011-09-20 11:28:35 -0400

    update urls for subdomain

--- urls.py
+++ urls.py
@@ -7,23 +7,22 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^180/login/$', 'one80.auth.views.login_begin', name='login'),
-    url(r'^180/login/complete/$', 'one80.auth.views.login_complete', name='login_complete'),
-    url(r'^180/logout/$', 'one80.auth.views.logout_begin', name='logout'),
-    url(r'^180/logout/complete/$', 'django.contrib.auth.views.logout', {'next_page': '/180/', 'redirect_field_name': 'next'}, name='logout_complete'),
-    url(r'^180/profile/$', 'one80.auth.views.profile', name='profile'),
-    url(r'^180/', include('social_auth.urls')),
+    url(r'^login/$', 'one80.auth.views.login_begin', name='login'),
+    url(r'^login/complete/$', 'one80.auth.views.login_complete', name='login_complete'),
+    url(r'^logout/$', 'one80.auth.views.logout_begin', name='logout'),
+    url(r'^logout/complete/$', 'django.contrib.auth.views.logout', {'next_page': '/180/', 'redirect_field_name': 'next'}, name='logout_complete'),
+    url(r'^profile/$', 'one80.auth.views.profile', name='profile'),
+    url(r'^', include('social_auth.urls')),
 
-    url(r'^180/committee/(?P<slug>[\w-]+)/$', 'one80.committees.views.committee_detail', name='committee_detail'),
+    url(r'^committee/(?P<slug>[\w-]+)/$', 'one80.committees.views.committee_detail', name='committee_detail'),
 
-    url(r'^180/(?P<slug>[\w-]+)/$', 'one80.committees.views.hearing_detail', name='hearing_detail'),
-    url(r'^180/(?P<slug>[\w-]+)/(?P<photo_id>\d+)/$', 'one80.photos.views.photo_detail', name='photo_detail'),
+    url(r'^(?P<slug>[\w-]+)/$', 'one80.committees.views.hearing_detail', name='hearing_detail'),
+    url(r'^(?P<slug>[\w-]+)/(?P<photo_id>\d+)/$', 'one80.photos.views.photo_detail', name='photo_detail'),
+    url(r'^(?P<slug>[\w-]+)/(?P<photo_id>\d+)/annotations.json$', 'one80.photos.views.photo_annotations', name='photo_annotations'),
+    url(r'^annotation/(?P<annot_id>\d+)/approve/$', 'one80.photos.views.approve_annotation', name='annotation_approve'),
+    url(r'^annotation/(?P<annot_id>\d+)/delete/$', 'one80.photos.views.delete_annotation', name='annotation_delete'),
 
-    url(r'^180/(?P<slug>[\w-]+)/(?P<photo_id>\d+)/annotations.json$', 'one80.photos.views.photo_annotations', name='photo_annotations'),
-    url(r'^180/annotation/(?P<annot_id>\d+)/approve/$', 'one80.photos.views.approve_annotation', name='annotation_approve'),
-    url(r'^180/annotation/(?P<annot_id>\d+)/delete/$', 'one80.photos.views.delete_annotation', name='annotation_delete'),
-
-    url(r'^180/$', 'one80.views.index', name='index'),
+    url(r'^$', 'one80.views.index', name='index'),
 )
 
 

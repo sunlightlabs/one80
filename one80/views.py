e722ed08da92f8e14fd63c76ac3dd9d2a88aefe0
Dan Drinkard <dan.drinkard@gmail.com>
2012-02-14 16:14:57 -0500

    tpl logic for new homepage

--- one80/views.py
+++ one80/views.py
@@ -12,10 +12,10 @@ from one80.photos.models import Photo, Annotation
 def index(request):
     hearings = Hearing.objects.published().select_related()
     context = {
-        'latest_hearings': hearings[:10],
-        'latest_annotations': Annotation.objects.published().select_related()[:3],
-        'committees': Committee.objects.all().select_related(),
-        'featured_photo': hearings[0].photos.all()[0],
+        'latest_hearings': hearings[:3],
+        # 'latest_annotations': Annotation.objects.published().select_related()[:3],
+        # 'committees': Committee.objects.all().select_related(),
+        # 'featured_photo': hearings[0].photos.all()[0],
         'leaderboard': Person.objects.with_counts(min_tags=1)[:5]
     }
 
